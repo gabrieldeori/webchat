0181d37ba292198e12648aa6bfbebaf61edf2fc2
gabrieldeori <deori.pessoal@gmail.com>
2022-01-20 16:39:57 -0300

    feat: Adicionado interações two-way de "message" em sockets/serverChat

--- sockets/serverChat.js
+++ sockets/serverChat.js
@@ -1,6 +1,13 @@
+const { getTimeAndFormat } = require('../utils');
+
 function chatSocket(io) {
   io.on('connection', (socket) => {
     console.log(socket.id);
+    socket.on('message', ({ chatMessage, nickname }) => {
+      const messageTimeStamps = getTimeAndFormat();
+      const formattedMessageToFront = `${messageTimeStamps} - ${nickname}: ${chatMessage}`;
+      io.emit('message', formattedMessageToFront);
+    });
   });
 }
 
